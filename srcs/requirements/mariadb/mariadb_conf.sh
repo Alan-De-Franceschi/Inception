33e263c404933aacc045295f5037c6bbc4118aa6
ade-fran <ade-fran@ade-fran.myguest.virtualbox.org>
2025-04-08 18:10:43 +0200

    up

---
+++ srcs/requirements/mariadb/mariadb_conf.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+service mariadb start
+
+mysql -e "CREATE DATABASE IF NOT EXISTS inception"
+
+mysql -e "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASS';"
+
+mysql -e "GRANT ALL ON inception.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASS';"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASS';"
+
+mysql -e "flush privileges;"
+
+mysqladmin -u root -p$SQL_ROOT_PASS shutdown
+
+exec mysqld_safe
