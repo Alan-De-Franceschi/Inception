c3466cf5e87b6bfc83c79bcef5bf6499d5772a90
ade-fran <ade-fran@ade-fran.myguest.virtualbox.org>
2025-04-16 17:25:03 +0200

    up

--- srcs/requirements/wordpress/tools/wp_conf.sh
+++ srcs/requirements/wordpress/tools/wp_conf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 10
+
 export SQL_PASS=$(cat /run/secrets/db_password)
 export WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_password)
 export WP_USER_PASS=$(cat /run/secrets/wp_user_password)
@@ -24,7 +26,7 @@ chown -R www-data:www-data /var/www/wordpress
 
 wp core download --allow-root
 
-wp core config --dbhost=mariadb:3306 -dbname="$SQL_DATABASE" --dbuser="$SQL_USER" --dbpass="$SQL_PASS" --allow-root
+wp core config --dbhost=mariadb:3306 --dbname="$SQL_DATABASE" --dbuser="$SQL_USER" --dbpass="$SQL_PASS" --allow-root
 
 wp core install --url="$WP_DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_MAIL" --allow-root
 
